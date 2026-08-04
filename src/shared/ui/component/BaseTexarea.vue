7105f0f5e55fb0c090a5574d0cdea9237d151c50
tinhtran-apa <tinh.tran@uniteness.one>
2026-08-04 22:21:27 +0700

    implement layout components and base UI elements

---
+++ src/shared/ui/component/BaseTexarea.vue
@@ -0,0 +1,14 @@
+<template>
+  <textarea
+    v-model="model"
+    class="py-2 px-3 rounded-xl border-2 border-border text-md bg-input w-full"
+  />
+</template>
+
+<script setup>
+const model = defineModel({
+  type: [String, Number],
+  default: '',
+})
+
+</script>
---
+++ src/shared/ui/component/dialog/DialogClose.vue
@@ -0,0 +1,17 @@
+<template>
+  <BaseButton
+    variant="transparent"
+    type="button"
+    @click="close"
+  >
+    <X class="text-primary w-4 h-4"/>
+  </BaseButton>
+</template>
+
+<script setup>
+import { X } from '@lucide/vue'
+import { BaseButton } from '..'
+import { inject } from 'vue';
+
+const close = inject("dialogClose");
+</script>
