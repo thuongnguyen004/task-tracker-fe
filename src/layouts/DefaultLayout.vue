571ce5c76a331ea01d14a2653152f07d8698cc51
Tran Ka Bun <bun2932005@gmail.com>
2026-08-07 16:12:20 +0700

    chore: add padding in default layout and header compoennt

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,26 +1,25 @@
 <template>
-  <div class="bg-gradient-surface flex h-screen flex-col overflow-auto">
+  <div class="bg-gradient-surface flex h-screen flex-col">
     <Header class="shrink-0" />
 
-    <main class="px-6 py-4">
+    <main class="px-6 py-4 overflow-auto">
       <RouterView v-slot="{ Component }">
-          <Transition
-            enter-active-class="transition-all duration-200 ease-out"
-            enter-from-class="opacity-0 scale-95"
-            enter-to-class="opacity-100 scale-100"
-            leave-active-class="transition-all duration-200 ease-out"
-            leave-from-class="opacity-100 scale-100"
-            leave-to-class="opacity-0 scale-95"
-            mode="out-in"
-          >
-            <component :is="Component" />
-          </Transition>
-        </RouterView>
+        <Transition
+          enter-active-class="transition-all duration-200 ease-out"
+          enter-from-class="opacity-0 scale-95"
+          enter-to-class="opacity-100 scale-100"
+          leave-active-class="transition-all duration-200 ease-out"
+          leave-from-class="opacity-100 scale-100"
+          leave-to-class="opacity-0 scale-95"
+          mode="out-in"
+        >
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
     </main>
   </div>
 </template>
 
 <script setup>
-import Header from './components/Header.vue';
-
+import Header from './components/Header.vue'
 </script>
--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex items-center justify-between bg-secondary p-3 border-b border-border">
+  <header class="flex items-center justify-between bg-secondary border-b border-border px-6 py-3">
     <div class="flex items-center gap-4">
       <span
         class="bg-primary text-secondary flex h-10 w-10 items-center justify-center rounded-xl text-lg font-bold"
