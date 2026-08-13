5d44989e90f1d2ffeae635d57c211f653fde5be8
Tran Ka Bun <bun2932005@gmail.com>
2026-08-13 14:38:11 +0700

    feat(auth): config redirect to dashboard url when user access auth path if having token

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ const router = createRouter({
     {
       path: path.auth.href,
       redirect: path.auth.login.href,
+      meta: { guestOnly: true },
     },
 
     {
@@ -42,8 +43,15 @@ const router = createRouter({
 router.beforeEach((to) => {
   const isAuthenticated = !!tokenStorage.get()
 
-  if (to.matched.some((record) => record.meta.requiresAuth) && !isAuthenticated) {
-    return { name: path.auth.login.name }
+  if (to.meta.guestOnly && isAuthenticated) {
+    return { name: path.task.board.name }
+  }
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    return {
+      path: path.auth.login.href,
+      query: { redirect: to.fullPath },
+    }
   }
 
   if (to.matched.some((record) => record.meta.requiresGuest) && isAuthenticated) {
