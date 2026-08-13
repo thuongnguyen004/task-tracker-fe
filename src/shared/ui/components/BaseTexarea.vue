daacd68c0c2825b832cea0012130ef7cf4b84682
tinhtran-apa <tinh.tran@uniteness.one>
2026-08-13 22:59:18 +0700

    fix: add resize-none class to BaseTexarea for better user experience

--- src/shared/ui/components/BaseTexarea.vue
+++ src/shared/ui/components/BaseTexarea.vue
@@ -2,7 +2,7 @@
   <textarea
     v-model="model"
     @input="emit('input', $event)"
-    class="border-border text-md bg-input w-full rounded-xl border-2 px-3 py-2"
+    class="border-border text-md bg-input w-full rounded-xl border-2 px-3 py-2 resize-none"
   />
 </template>
 
